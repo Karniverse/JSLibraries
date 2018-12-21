cc17c549ca133f2bc7a34e701908e7a895fa1d16
mukil-bluerider <34996794+mukil-bluerider@users.noreply.github.com>
2018-12-21 04:03:52 -0500

    Update HTMLdoc.js

--- HTMLdoc.js
+++ HTMLdoc.js
@@ -28,9 +28,9 @@ var a5 = document.createElement('script');
 a5.src = "https://raw.githack.com/mukil-bluerider/JSLibraries/master/mammoth.js";
 document.getElementsByTagName('head')[0].appendChild(a5); 
   
-var a5 = document.createElement('script');
-a5.src = "https://raw.githack.com/mukil-bluerider/JSLibraries/master/Snow.js";
-document.getElementsByTagName('head')[0].appendChild(a5); 
+var a6 = document.createElement('script');
+a6.src = "https://raw.githack.com/mukil-bluerider/JSLibraries/master/Snow.js";
+document.getElementsByTagName('head')[0].appendChild(a6); 
   
 var sty = document.createElement('link');
 sty.rel="stylesheet"
